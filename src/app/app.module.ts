8b8c053cb5dbad674b570179f7659728f95db03f
miichaels <michael_araujo2012@hotmail.com>
2022-07-25 22:12:08 -0300

    Adicionando componentes - Navbar - footer - e Home iniciado o curriculo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RodapeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
